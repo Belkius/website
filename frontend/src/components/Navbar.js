f2a3d97543f94f6ae1425918c57f66e547d92ece
Belkius <118485347+Belkius@users.noreply.github.com>
2023-03-19 21:46:42 +0100

    problem with text hover color

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -53,19 +53,19 @@ function Navbar() {
                   <span className="text-2xl text-white font-semibold">Belkius</span>
               </a>
           <div className="hidden md:flex p-4 border border-none rounded-lg bg-transparent text-sm font-medium" id="navbar-pc">
-            <NavLink exact to="/" onClick={handleLinkClick} className={`px-3 py-2 text-[#fffffe]  hover:text-[#22d3ee] ${activeLink === '/' ? 'bg-[#22d3ee] hover:text-[#fffffe]' : ''}`}>
+            <NavLink exact to="/" onClick={handleLinkClick} className={`px-3 py-2 text-[#fefefe]  hover:text-[#22d3ee] ${activeLink === '/' ? 'bg-[#22d3ee] hover:text-[#fefefe]' : ''}`}>
               Projects
             </NavLink>
-            <NavLink exact to="/about" onClick={handleLinkClick} className={`px-3 py-2 text-[#fffffe]  hover:text-[#c4344f] ${activeLink === '/about' ? 'bg-[#c4344f] hover:text-[#fffffe]' : ''}`}>
+            <NavLink exact to="/about" onClick={handleLinkClick} className={`px-3 py-2 text-[#fefefe]  hover:text-[#c4344f] ${activeLink === '/about' ? 'bg-[#c4344f] hover:text-[#fefefe]' : ''}`}>
               About Me
             </NavLink>
-            <NavLink to="/skills" onClick={handleLinkClick} className={`px-3 py-2 text-[#fffffe]  hover:text-[#22d3ee] ${activeLink === '/skills' ? 'bg-[#22d3ee] hover:text-[#fffffe]' : ''}`}>
+            <NavLink to="/skills" onClick={handleLinkClick} className={`px-3 py-2 text-[#fefefe]  hover:text-[#22d3ee] ${activeLink === '/skills' ? 'bg-[#22d3ee] hover:text-[#fefefe]' : ''}`}>
               Skills
             </NavLink>
-            <NavLink to="/info" onClick={handleLinkClick} className={`px-3 py-2 text-[#fffffe]  hover:text-[#c4344f] ${activeLink === '/info' ? 'bg-[#C4344F] hover:text-[#fffffe]' : ''}`}>
+            <NavLink to="/info" onClick={handleLinkClick} className={`px-3 py-2 text-[#fefefe]  hover:text-[#c4344f] ${activeLink === '/info' ? 'bg-[#C4344F] hover:text-[#fefefe]' : ''}`}>
               Website Info
             </NavLink>
-            <NavLink to="/contact" onClick={handleLinkClick} className={`px-3 py-2 text-[#fffffe]  hover:text-[#22d3ee] ${activeLink === '/contact' ? 'bg-[#22d3ee] hover:text-[#fffffe]' : ''}`}>
+            <NavLink to="/contact" onClick={handleLinkClick} className={`px-3 py-2 text-[#fefefe]  hover:text-[#22d3ee] ${activeLink === '/contact' ? 'bg-[#22d3ee] hover:text-[#fefefe]' : ''}`}>
               Contact
             </NavLink>
           </div>
